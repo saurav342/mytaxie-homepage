f83b018f22cf90b0b9e0f198e05acee5895a7df8
rohit raj <richaimsinha+gh@gmail.com>
2023-04-01 13:27:24 +0530

    changes in data and flight details

--- src/Screens/FlightDetails.jsx
+++ src/Screens/FlightDetails.jsx
@@ -3,66 +3,98 @@ import "react-phone-number-input/style.css";
 import PhoneInput from "react-phone-number-input";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import {flightData} from "../Utils/constants.js"
+import { flightData } from "../Utils/constants.js";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const FlightDetails = () => {
-  console.log("this data.......", flightData);
   const navigate = useNavigate();
-  const [fromCity, setFromCity] = useState("From ....");
-  const [toCity, setToCity] = useState("To....");
-  const [cityList, setCityList] = useState(flightData);
+  const [fromCity, setFromCity] = useState("");
+  const [toCity, setToCity] = useState("");
+  const [fromCityList, setFromCityList] = useState(flightData);
+  const [toCityList, setToCityList] = useState(flightData);
   const [phoneNum, setValue] = useState();
-  const [flightDate, setFlightDate] = useState("");
+  const [startDate, setStartDate] = useState();
 
   const handleLength = (e) => {
     if (e.target.value.length > e.target.maxLength) {
       setValue("enter valid number");
     }
-  };
+  };  
 
   const handleFromCity =(e) =>{
     setFromCity(e.target.value)
-    var cityListArr = cityList;
-
+    var fromCityListArr = flightData;
     var newarr = [];
-    console.log("........fromCity",fromCity)
-    
-    for (let i = 0; i < cityListArr.length; i++) {
-      console.log("........fromCity", fromCity);
+    for (let i = 0; i < fromCityListArr.length-1; i++) {
+      if (e.target.value != fromCityListArr[i]) {
+        newarr = [...newarr, fromCityListArr[i]];
+      }
+    }
+    setToCityList(newarr);
+  };
+  
+  const handleToCity =(e)=>{
+    setToCity(e.target.value)
+    var cityListArr = flightData;
+    var newarr = [];
+    for (let i = 0; i < cityListArr.length-1; i++) {
       if (e.target.value != cityListArr[i]) {
         newarr = [...newarr, cityListArr[i]];
       }
     }
-    setCityList(newarr);
-  console.log("................",newarr)
+    setFromCityList(newarr);
   }
 
-  const handleValidation = () => {
+  const handleData = (e) => {
+    e.preventDefault();
     if (
       fromCity === "From ...." ||
       toCity === "To...." ||
       phoneNum === null ||
-      flightDate === ""
+      startDate === ""
     ) {
       return false;
-    } else return true;
-  };
+    } else {
+      const backendUrl = "https://api.mytaxie.com/v1/flight";
+      const whatsappCBUrl =
+        "https://graph.facebook.com/v16.0/113290301716804/messages";
+      const headers = {
+        "Content-type": "application/json",
+        Authorization:
+          "Bearer EAAOh1MtDJVQBAPUaWHHpUubrKhEC7GsHZCSqIERazBXTj3zkuFd6Hc4tBco7TP10u9p77xDFS9M10CFgthham70X477UHFKqQgYKF212pwYCwjWs7T6BbMqMMZCFP18h4wHSfTqa0pZAx7y6lgMcCp55UiyqOwUFyCVXPsMPVLaCWOTV4ik92BMZCwdZBd84f0GZBigM5oWwZDZD",
+      };
+      const dataBodyForWhatsappMsg = {
+        messaging_product: "whatsapp",
+        to: "919155524079",
+        type: "template",
+        template: {
+          name: "hello_world",
+          language: {
+            code: "en_US",
+          },
+        }    
+      };
 
-  const handleData = (e) => {
-    e.preventDefault();
-    axios
-      .post("https://api.mytaxie.com/v1/flight", {
-        from: fromCity,
-        to: toCity,
-        phoneNumber: phoneNum,
-        date: flightDate,
-      })
-      .then(function (response) {
-        console.log("...........response", response);
-      })
-      .finally(function () {
-        navigate("/flightsuccess");
-      });
+      axios
+        .all([
+          axios.post(backendUrl, {
+            from: fromCity,
+            to: toCity,
+            phoneNumber: phoneNum,
+            date: startDate,
+          }),
+          axios.post(whatsappCBUrl, dataBodyForWhatsappMsg, {
+            headers: headers,
+          }),
+        ])
+        .then(function (response) {
+          console.log("...........response", response);
+        })
+        .finally(function () {
+          navigate("/flightsuccess");
+        });
+    }
   };
 
   return (
@@ -84,12 +116,17 @@ const FlightDetails = () => {
                 <div className="form-group">
                   <select
                     className="ajaxField flightDetails"
-                    onChange={handleFromCity}
-                    value=""
-                    required
+                    // onChange={handleFromCity}
+                    onClick={handleFromCity}
+                    value={fromCity}
+                    isOptionDisabled = {true}
+
+                    // defaultValue = {"From...."}
+                    // required
                   >
-                    <option value="From"> {fromCity}</option>
-                    {cityList.map((city, index) => (
+                    {/* <option value="" selected disabled> {fromCity}</option> */}
+                    {/* <option  selected disabled> From...</option> */}
+                    {fromCityList.map((city, index) => (
                       <option>{city}</option>
                     ))}
                   </select>
@@ -99,11 +136,13 @@ const FlightDetails = () => {
                 <div className="form-group">
                   <select
                     className="ajaxField flightDetails"
-                    value=""
-                    onChange={(e) => setToCity(e.target.value)}
+                    value={toCity}
+                    // onChange={handleToCity}
+                    onClick={handleToCity}
                   >
-                    <option value="">{toCity}</option>
-                    {cityList.map((city, index) => (
+                    {/* <option value="" selected disabled>{toCity}</option> */}
+                    {/* <option selected disabled>To...</option> */}
+                    {toCityList.map((city, index) => (
                       <option>{city}</option>
                     ))}
                   </select>
@@ -125,11 +164,13 @@ const FlightDetails = () => {
               </div>
               <div className="col-md-5ths">
                 <div className="form-group">
-                  <input
-                    type="date"
-                    onChange={(e) => setFlightDate(e.target.value)}
-                    value={flightDate}
-                    className="flightDetails ajaxFeild"
+                  <DatePicker
+                    selected={startDate}
+                    onChange={(date) => setStartDate(date)}
+                    minDate={new Date()}
+                    placeholderText="Select a date"
+                    showTimeSelect
+                    dateFormat="MMMM d, yyyy h:mm aa"
                   />
                 </div>
               </div>
